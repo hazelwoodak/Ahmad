27b84ba7e11c8d0ead94c5c4b18f8c45cad1ffd3
hazelwoodak <78044801+hazelwoodak@users.noreply.github.com>
2021-01-26 13:58:07 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/************************************************************************Hello
+**************************************************************************Ahmad Hazelwood
+*01/26/2021
+* Course 255 Section 003
+************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	System.out.println("Ahmad Hazelwood");
+	System.out.println("Hello");
+	System.out.println("Running a basic Java script as an introduction to the class.");
+	System.out.println("01/26/2021");
+	System.out.println("Course 255 Section 003");
+	System.out.println();
+	}
+}
